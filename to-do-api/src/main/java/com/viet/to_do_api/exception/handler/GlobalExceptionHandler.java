4e131c47fe94f07d9d49d112c311acd9651610d8
nguyendangviet12022004 <viet.ngdang.dev@gmail.com>
2025-09-23 06:41:44 +0700

    fix(auth) TDL-15 fix invalid method argument exception for global

--- to-do-api/src/main/java/com/viet/to_do_api/exception/handler/GlobalExceptionHandler.java
+++ to-do-api/src/main/java/com/viet/to_do_api/exception/handler/GlobalExceptionHandler.java
@@ -1,19 +1,50 @@
 package com.viet.to_do_api.exception.handler;
 
 import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.viet.to_do_api.dto.exception.ExceptionResponse;
 import com.viet.to_do_api.exception.auth.EmailExistsException;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.var;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    @Nullable
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(@NonNull MethodArgumentNotValidException ex,
+            @NonNull HttpHeaders headers, @NonNull HttpStatusCode status, @NonNull WebRequest request) {
+
+        // get message string
+        String message = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> String.format("%s: %s", error.getField(), error.getDefaultMessage()))
+                .collect(Collectors.joining("\n"));
+
+        var response = ExceptionResponse.builder()
+                .message(message)
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST)
+                .path(((ServletWebRequest) request).getRequest().getRequestURI())
+                .build();
+        return ResponseEntity.badRequest().body(response);
+    }
 
     @ExceptionHandler(exception = EmailExistsException.class)
     public ResponseEntity<ExceptionResponse> emailExistExceptionHanlder(EmailExistsException exception,
