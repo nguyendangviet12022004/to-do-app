c181818b9a2d989106dfd67c0e42fa9bee1fe4d0
nguyendangviet12022004 <viet.ngdang.dev@gmail.com>
2025-10-16 22:01:37 +0700

    feat(login,ui):  TDL-22 add login ui - to do : fix enter wrong password - to do: fix disable account

--- to-do-ui/src/app/pages/auth/login/login.ts
+++ to-do-ui/src/app/pages/auth/login/login.ts
@@ -1,15 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { LoginRequest } from '../../../models/auth/login.request';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../../services/auth.service';
+import { catchError, of, switchMap, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
   loginRequest: LoginRequest = {
     email: "",
     password: ""
+  }
+
+  errorMessage = signal<string>("");
+
+  onLogin(){
+    this.authService.login(this.loginRequest).pipe(
+      catchError((err) => {
+        if(err.error?.code === "DISABLED_ACCOUNT"){
+          return this.authService.getActivateCode(this.loginRequest.email).pipe(
+            switchMap(() => {
+              this.router.navigate(['/auth/activate'], {queryParams: {email: this.loginRequest.email}});
+              return of(null);
+            })
+          )
+        }
+
+        return throwError(() => err)
+      })
+    ).subscribe({
+      next: (response) => {
+       this.authService.saveTokens(response!.accessToken, response!.refreshToken);
+       this.router.navigate(['/']);
+      },
+      error: (err) => {
+        this.errorMessage.set(err.error?.message || "An error occurred during login. Please try again.");
+      }
+    })
   }
 }
--- to-do-ui/src/app/services/auth.service.ts
+++ to-do-ui/src/app/services/auth.service.ts
@@ -1,15 +1,20 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../environments/enviroment.dev';
 import { RegisterRequest } from '../models/auth/register.request';
 import { Observable } from 'rxjs';
 import { ActivateRequest } from '../models/auth/activate.request';
+import { LoginRequest } from '../models/auth/login.request';
+import { LoginResponse } from '../models/auth/login.response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private readonly httpClient = inject(HttpClient)
+
+  private refreshTokenSignal = signal<string>("");
+  private accessTokenSignal = signal<string>("");
 
   register(request: RegisterRequest): Observable<any>{
     return this.httpClient.post(`${environment.apiUrl}/auth/register`,request)
@@ -27,4 +32,16 @@ export class AuthService {
     let params:HttpParams = new HttpParams().set('email', email);
     return this.httpClient.get(`${environment.apiUrl}/auth/activate-account-code`, {params: params})
   }
+
+  login(request: LoginRequest): Observable<LoginResponse>{
+    return this.httpClient.post<LoginResponse>(`${environment.apiUrl}/auth/login`, request)
+  }
+
+  saveTokens(accessToken: string, refreshToken: string): void {
+    this.accessTokenSignal.set(accessToken);
+    this.refreshTokenSignal.set(refreshToken);
+    localStorage.setItem('access_token', accessToken);
+    localStorage.setItem('refresh_token', refreshToken);
+  }
+  
 }
