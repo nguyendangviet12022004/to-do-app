b2e8ea642cd0786feb3392fb778016d7452eac33
nguyendangviet12022004 <viet.ngdang.dev@gmail.com>
2025-11-07 10:19:58 +0700

    chore(config): move from local to git config file repo

--- to-do-api/src/main/java/com/viet/to_do_api/controller/TestController.java
+++ to-do-api/src/main/java/com/viet/to_do_api/controller/TestController.java
@@ -2,6 +2,9 @@ package com.viet.to_do_api.controller;
 
 import java.util.Map;
 
+import com.viet.to_do_api.dto.config.BuildInfoDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,13 +19,23 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/test")
 @RequiredArgsConstructor
+@EnableConfigurationProperties(value = {BuildInfoDto.class})
 public class TestController {
 
     private final MailService mailService;
+    private final BuildInfoDto buildInfoDto;
+
+
+
 
     @GetMapping("/mail")
     public ResponseEntity<?> sendMail() throws MessagingException {
         mailService.sendHtmlMail("viet.ngdang.dev@gmail.com", "Test html", "mail/activate-code", Map.of("code", 54987));
         return ResponseEntity.ok("Mail sent");
+    }
+
+    @GetMapping("/build-info")
+    public ResponseEntity<?> buildInfo()  {
+        return ResponseEntity.ok(buildInfoDto);
     }
 }
--- to-do-api/src/main/java/com/viet/to_do_api/dto/config/BuildInfoDto.java
+++ to-do-api/src/main/java/com/viet/to_do_api/dto/config/BuildInfoDto.java
@@ -1,7 +1,18 @@
 package com.viet.to_do_api.dto.config;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties("")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ConfigurationProperties("build")
 public class BuildInfoDto {
+    private String version;
+
+    private String environment;
 }
